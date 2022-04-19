79474b00706f9e2977dc530682be0cac872e1d7d
gmakc-094423 <gmakc-094423@yandex.ru>
2022-04-20 01:38:33 +0300

    Виды методов

--- example_01/Program.cs
+++ example_01/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// вид 1. Ничего не получают, ничего не возвращают
+void Method1(){
+  Console.WriteLine($"Первый метод");
+}
+// Method1();
+
+// вид 2. Получают, но не ниччего не возвращают
+void Method2(string message){
+  Console.WriteLine($"Второй метод {message}");
+}
+// Method2("Это 2-й метод");
+
+// вид 3. Возвращают, но не получают
+int Method3(){
+  return DateTime.Now.Year; 
+}
+int year = Method3();
+// Console.WriteLine(year);
+
+// вид 4. И возвращают и получают
+string Method4(int count, string c){
+  string result = String.Empty;
+  int i = 0;
+  while (i < count){
+    Console.WriteLine(c);
+    i++;
+  }
+  return result;
+}
+string res = Method4(5, "Это метод 4");
