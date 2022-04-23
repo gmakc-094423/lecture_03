8cbb75141bdea033f200f031be4f8a21e8406ca6
gmakc-094423 <gmakc-094423@yandex.ru>
2022-04-24 02:19:47 +0300

    Доработал лекционный материал

--- example_01/Program.cs
+++ example_01/Program.cs
@@ -29,4 +29,84 @@ string Method4(int count, string text){
 }
 string res = Method4(text: "Это метод 4", count: 5);
 
- Console.WriteLine(res);
+//  Console.WriteLine(res);
+
+
+/* for (int i=1; i<10;i++){
+  Console.WriteLine($"\nУмножение на {i}");
+  for (int j=1; j<10;j++){
+    Console.Write($"{i}*{j}={i*j}; ");
+  }
+} */
+
+/* string text = " Начальник войск радиационной, химической и биологической защиты"
++ " ВС РФ генерал-лейтенант Игорь Кириллов сообщил о трёх сценариях, которые "
++"США могут использовать для обвинения России в использовании оружия массового "+"уничтожения (ОМУ) на Украине. ";
+
+string Replace(string text, char oldValue, char newValue){
+  string result=String.Empty;
+  int length = text.Length; 
+  for (int i = 0; i < length; i++)
+  {
+    if (text[i]==oldValue) result =result + $"{newValue}";
+    else result = result + $"{text[i]}";
+  }
+  return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.Write(newText);
+
+newText = Replace(newText, 'к', 'К');
+Console.Write(newText); */
+
+
+int[] numbers = new int[20];
+
+int[] FillIntArray (int[] array) {
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(0, 1000);
+    // Console.Write(numbers[i] + ", ");
+  }
+  return array;
+}
+
+int[] array = FillIntArray(numbers);
+
+void PrintArray (int[] arr){
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
+  Console.WriteLine($"");
+}
+
+void SelectionSort(int[] array){
+  for (int i = 0; i < array.Length-1; i++)
+  {
+    int minPosition = i;
+
+    for (int j = i+1; j < array.Length; j++)
+    {
+      if (array[j] > array[minPosition]) minPosition = j;
+    }
+    int temporary = array[i];
+    array[i] = array[minPosition];
+    array[minPosition] = temporary;
+  }
+
+}
+
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
+
+
+
+// int RandomNumber(int min, int max){   // функция получения рандомных чисел
+//   return new Random().Next(min, max);
+// }
+
+// int number = RandomNumber(10, 100);
