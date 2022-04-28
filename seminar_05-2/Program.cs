1e58e1125b52d042e786e98dcdef8a5dae19de24
gmakc-094423 <gmakc-094423@yandex.ru>
2022-04-28 19:48:26 +0300

    Домашняя работа

---
+++ seminar_05-2/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+
+Console.Clear();
+
+Console.WriteLine($"Задача 35. 123 случайных числа. Диапазон от -1000 до 1000");
+
+int[] numbers = new int[123];
+FillArray(numbers, -1000, 1000);
+
+void FillArray(int[] array, int min, int max){
+  for (int i = 0; i<array.Length; i++ ){
+    array[i] = new Random().Next(min, max);
+  }
+}
+
+void WriteArray(int[] array){
+    for (int i = 0; i<array.Length; i++ ){
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
+
+int count = 0;
+for(int i = 0; i<numbers.Length; i++ )
+{
+  if (numbers[i]>=10 && numbers[i]<100)
+    {
+      count += 1;
+    }
+}
+Console.WriteLine($"Число элементов от 10 до 99: {count} \n");
+
+
+
+  // Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+
+Console.WriteLine($"Задача 37");
+
+int[] numbers1 = new int[6];
+
+int IsEven(int[] numbers1){
+  int middle = numbers1.Length / 2;
+  if (middle % 2 != 1){
+    middle = middle + 1;
+  }
+  return middle;
+}
+
+int middle = IsEven(numbers1);
+
+int[] multiplication = new int[middle];
+FillArray(numbers1, 1, 10);
+
+for(int i = 0; i < middle; i++ )
+{
+  multiplication[i] = numbers1[i] * numbers1[numbers1.Length-i-1];
+
+}
+
+if ((numbers1.Length / 2) % 2 != 1){
+  multiplication[middle-1] = numbers1[middle-1];
+}
+
+WriteArray(numbers1);
+WriteArray(multiplication);
--- seminar_05/Program.cs
+++ seminar_05/Program.cs
@@ -1,1 +1,66 @@
-﻿
+﻿// сумма положительны . Сумма отрицательных
+Console.Clear();
+int[] numbers = new int[12];
+FillArray(numbers);
+WriteArray(numbers);
+/* 
+int sumPositive = 0;
+int sumNegative = 0;
+for(int i = 0; i<numbers.Length; i++ ){
+  if (numbers[i]>0){
+    sumPositive +=  numbers[i];
+  }
+  else sumNegative += numbers[i];
+}
+Console.WriteLine(sumPositive);
+Console.WriteLine(sumNegative); */
+
+void FillArray(int[] array){
+  for (int i = 0; i<array.Length; i++ ){
+    array[i] = new Random().Next(-9,10);
+  }
+}
+
+void WriteArray(int[] array){
+    for (int i = 0; i<array.Length; i++ ){
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
+
+// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+for(int i = 0; i<numbers.Length; i++ ){
+  numbers[i] = numbers[i] * (-1);
+  }
+WriteArray(numbers);
+
+
+
+
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+// Console.Clear();
+Console.WriteLine();
+int[] numbers1 = new int[12]{-1, -8, -1, 5, 4, -8, -2, 5, 0, -1, 1, -7};
+
+Console.Write("Введите проверяемое число: ");
+int proverNumbers = Convert.ToInt32(Console.ReadLine());
+
+int temp = 0;
+
+for (int i=0; i< numbers.Length; i++)
+{
+  if(numbers1[i] == proverNumbers)
+  {
+    temp = 1;
+    break;
+  }
+}
+
+if (temp == 1)
+{
+  Console.WriteLine($"Проверяемое {proverNumbers} число есть!");
+}
+else Console.WriteLine($"Проверяемого  {proverNumbers} числа нет.");
